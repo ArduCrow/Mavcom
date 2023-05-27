e8d7c550f4a0cf57d21ed624558d4048f0aaa0c5
BBB <BBB@BBB.com>
2023-05-27 01:16:03 +0100

    tests updated

--- tests/test_mavcom.py
+++ tests/test_mavcom.py
@@ -1,17 +1,20 @@
 from mavcom import mavcom
-import multiprocessing
+from multiprocessing import Process
 import time
 import os
+import atexit
 
 def sim_vehicle():
-    os.system("sim_vehicle.py -v ArduCopter")
+    os.system("gnome-terminal --tab -e 'bash -c \"sim_vehicle.py -v ArduCopter; exec bash\"'")
 
-sim = multiprocessing.Process(target=sim_vehicle)
+sim = Process(target=sim_vehicle, daemon=True)
 sim.start()
+print(sim.pid)
 
 vehicle = mavcom.Mavcom(
     connection_path="127.0.0.1:14551"
 )
+vehicle.start()
 time.sleep(1)
 
 def test_receiving_mavlink_packets():
@@ -20,17 +23,32 @@ def test_receiving_mavlink_packets():
 def test_vehicle_ready():
     for i in range(20):
         if vehicle.ready:
-            assert True
+            break
         else:
-            time.sleep(1)
-            continue
-    assert False
+            time.sleep(2)
+            pass
+    assert vehicle.ready
     
 def test_set_flight_mode():
     vehicle.flight_mode = "GUIDED"
     assert vehicle.flight_mode == "GUIDED"
     
+def test_arm_motors():
+    vehicle.motors_armed = True
+    time.sleep(1)
+    assert vehicle.motors_armed
+    
 def test_takeoff():
     vehicle.takeoff(alt=5)
-    time.sleep(5)
+    for i in range(11):
+        if vehicle.vehicle_state.alt < 4.9:
+            time.sleep(1)
+        else: 
+            break
     assert vehicle.vehicle_state.alt >= 4.9
+    
+def test_sim_terminated():
+    sim.join()
+    os.system(f"kill {sim.pid}")
+    time.sleep(2)
+    assert not sim.is_alive()
